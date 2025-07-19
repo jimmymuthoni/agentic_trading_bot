0df89b3b94580685382623dbcbb17bc936e4260f
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-19 22:02:30 +0300

    project setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from typing import List
+
+# function to get all requirements
+def get_requirements() ->List:
+    requirements: List[str] = []
+    try:
+        with open("requirements.txt", 'r') as file:
+            lines = file.readline()
+            for line in lines:
+                requirement = line.strip()
+                if requirement and requirement != '-e .':
+                    requirements.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    return requirements
+print(get_requirements())
+
+#project set up
+setup(
+    name = "trading_bot",
+    version = "0.0.1",
+    author="jimmy muthoni",
+    author_email="jimmymuthoni26@gmail.com",
+    packages=find_packages(),
+    install_reqires = get_requirements(),
+    python_requires = ">3.10",
+)
