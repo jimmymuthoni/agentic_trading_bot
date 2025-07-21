3b6de2b79dc6f30a015cf475b5d6a0839fbf1233
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-21 12:48:08 +0300

    retriever tool

---
+++ toolkit/tools.py
@@ -0,0 +1,32 @@
+import os
+from langchain.tools import tool
+from langchain_community.tools import TavilySearchResults
+from langchain_community.tools.polygon.financials import PolygonFinancials
+from langchain_community.utilities.polygon import PolygonAPIWrapper
+from langchain_community.tools.bing_search import BingSearchResults
+from data_models.models import RagToolSchema
+from langchain_pinecone import PineconeVectorStore
+from utils.model_loader import ModelLoader
+from utils.config_loader import load_config
+from dotenv import load_dotenv
+from pinecone import Pinecone
+load_dotenv()
+
+api_wraper = PolygonAPIWrapper()
+model_loader = ModelLoader()
+config = load_config()
+
+#tool for retieving information from pineconedb
+@tool(args_schema=RagToolSchema)
+def retiever_tool(question):
+    """This is a retiever tool"""
+    pinecone_api_key = os.getenv("PINECONE_API_KEY")
+    pc = Pinecone(api_key=pinecone_api_key)
+    vector_store = PineconeVectorStore(index=pc.Index(config["vector_db"]["index_name"]), embedding=model_loader.groq_api_key())
+    retriever = vector_store.as_retriever(
+        search_type = "similarity_score_threshold",
+        search_kwargs = {"k": config["retriever"]["top_k"], "score_threshold":config["retriever"]["score_threshold"]}
+
+    )
+
+    return retriever
