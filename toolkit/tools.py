3c7117c8162b101759b10ee3001c0ffbb260d083
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-21 12:59:06 +0300

    tools for the agent

--- toolkit/tools.py
+++ toolkit/tools.py
@@ -3,7 +3,6 @@ from langchain.tools import tool
 from langchain_community.tools import TavilySearchResults
 from langchain_community.tools.polygon.financials import PolygonFinancials
 from langchain_community.utilities.polygon import PolygonAPIWrapper
-from langchain_community.tools.bing_search import BingSearchResults
 from data_models.models import RagToolSchema
 from langchain_pinecone import PineconeVectorStore
 from utils.model_loader import ModelLoader
@@ -19,7 +18,7 @@ config = load_config()
 #tool for retieving information from pineconedb
 @tool(args_schema=RagToolSchema)
 def retiever_tool(question):
-    """This is a retiever tool"""
+    """This is a retiever tool for interacting with pinecone vector db"""
     pinecone_api_key = os.getenv("PINECONE_API_KEY")
     pc = Pinecone(api_key=pinecone_api_key)
     vector_store = PineconeVectorStore(index=pc.Index(config["vector_db"]["index_name"]), embedding=model_loader.groq_api_key())
