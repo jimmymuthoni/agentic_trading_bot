c52d839c55a0de3d9d37363e86cb5288be346aaa
jimmymuthoni <jimmymuthoni26@gmail.com>
2025-07-22 16:55:26 +0300

    backend server

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, UploadFile, File, Request
+from fastapi.middleware.cors import CORSMiddleware
+from typing import List
+from starlette.responses import JSONResponse
+from data_ingestion.data_ingestion_pipeline import DataIngestion
+from agent.workflow import GraphBuilder
+from data_models.models import *
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
+
+@app.post("/upload")
+async def upload_files(files: List[UploadFile] = File(...)):
+    try:
+        ingestion = DataIngestion()
+        ingestion.run_pipeline(files)
+        return {"message": "Files successfully processed and stored."}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+    
+@app.post("/query")
+async def query_chatbot(request: QuestionRequest):
+    try:
+        graph_service = GraphBuilder()
+        graph_service.build()
+        graph = graph_service.get_graph()
+        messages={"messages": [request.question]}
+        
+        result = graph.invoke(messages)
+        if isinstance(result, dict) and "messages" in result:
+            final_output = result["messages"][-1].content
+        else:
+            final_output = str(result)
+        
+        return {"answer": final_output}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
